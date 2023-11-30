44a5b2d122b479137850e6256b6a8fefedce8e5c
sghufranmehdi <sgmehdi110@gmail.com>
2023-11-30 23:38:02 +0500

    basic routes and pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./App.css";
+import Layout from "./Layouts/Layout";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Layout />
     </>
   );
 }
