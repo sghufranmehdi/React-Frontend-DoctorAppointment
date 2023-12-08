45da631e931b695b1bc95737b1e3c8a38f2c1adb
sghufranmehdi <sgmehdi110@gmail.com>
2023-12-08 06:31:38 +0500

    feature section complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import About from "../../Components/About/About";
 import { Hero } from "../../Components/Hero/Hero";
 import MidHome from "../../Components/Midhome/MidHome";
 import ServiceSection from "../../Components/ServiceSection/ServiceSection";
+import Feature from "../../Components/Feature/Feature";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <MidHome />
       <About />
       <ServiceSection />
+      <Feature />
     </>
   );
 };
