953a65460595d827bafbbcaf8cced95378f510ad
sghufranmehdi <sgmehdi110@gmail.com>
2023-12-09 02:13:53 +0500

    doctors section completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Hero } from "../../Components/Hero/Hero";
 import MidHome from "../../Components/Midhome/MidHome";
 import ServiceSection from "../../Components/ServiceSection/ServiceSection";
 import Feature from "../../Components/Feature/Feature";
+import DoctorsSection from "../../Components/DoctorsSection/DoctorsSection";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <About />
       <ServiceSection />
       <Feature />
+      <DoctorsSection />
     </>
   );
 };
